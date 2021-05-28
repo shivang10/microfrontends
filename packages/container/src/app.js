1fd2d2182a64bf1f4b64111c1b1f8a8f8ea0ab44
Shivang Garg <shivang10garg@gmail.com>
2021-05-28 06:57:03 +0530

    Initial commit

---
+++ packages/container/src/app.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import MarketingApp from './components/marketingApp';
+
+export default () => {
+    return (
+        <div>
+            Container
+            <MarketingApp />
+        </div>
+    )
+};
