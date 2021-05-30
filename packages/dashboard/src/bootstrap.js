9d75e5787ce25f15ea7e8e8708d5e5f30abaa557
Shivang Garg <shivang10garg@gmail.com>
2021-05-30 20:03:20 +0530

    dashboard page added

---
+++ packages/dashboard/src/bootstrap.js
@@ -0,0 +1,22 @@
+import { createApp } from 'vue';
+import Dashboard from './components/Dashboard.vue';
+
+// mount function to start up the app
+const mount = (el) => {
+    const app = createApp(Dashboard);
+    app.mount(el);
+};
+
+// if we are in dev and in isolation,
+// call mount immediately
+if (process.env.NODE_ENV === 'development') {
+    const devRoot = document.querySelector('#_dashboard-dev-root');
+
+    if (devRoot) {
+        mount(devRoot);
+    }
+}
+
+// we are running through container
+// and we should export the mount function
+export { mount };
