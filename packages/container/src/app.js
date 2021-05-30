8c680e1e0444096016fd3f62c6bbcf67ffcfcc8b
Shivang Garg <shivang10garg@gmail.com>
2021-05-30 07:18:21 +0530

    addes header

--- packages/container/src/app.js
+++ packages/container/src/app.js
@@ -1,11 +1,15 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
 import MarketingApp from './components/marketingApp';
+import Header from './components/Header';
 
 export default () => {
     return (
-        <div>
-            Container Page
-            <MarketingApp />
-        </div>
-    )
+        <BrowserRouter>
+            <div>
+                <Header />
+                <MarketingApp />
+            </div>
+        </BrowserRouter>
+    );
 };
