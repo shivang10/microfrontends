8240b4e8ec17036f5559e32673ad683e8bb689f4
Shivang Garg <shivang10garg@gmail.com>
2021-05-30 18:45:53 +0530

    added authentication

--- packages/container/src/app.js
+++ packages/container/src/app.js
@@ -1,21 +1,36 @@
-import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
+import React, { lazy, Suspense, useState } from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import { StylesProvider, createGenerateClassName } from '@material-ui/styles';
 
-import MarketingApp from './components/marketingApp';
+import Progress from './components/Progress';
 import Header from './components/Header';
+
+const MarketingLazy = lazy(() => import('./components/marketingApp'));
+const AuthLazy = lazy(() => import('./components/authApp'));
 
 const generateClassName = createGenerateClassName({
     productionPrefix: 'co',
 });
 
 export default () => {
+    const [isSignedIn, setIsSignedIn] = useState(false);
+
     return (
         <BrowserRouter>
             <StylesProvider generateClassName={generateClassName}>
                 <div>
-                    <Header />
-                    <MarketingApp />
+                    <Header
+                        onSignOut={() => setIsSignedIn(false)}
+                        isSignedIn={isSignedIn}
+                    />
+                    <Suspense fallback={<Progress />}>
+                        <Switch>
+                            <Route path="/auth">
+                                <AuthLazy onSignIn={() => setIsSignedIn(true)} />
+                            </Route>
+                            <Route path="/" component={MarketingLazy} />
+                        </Switch>
+                    </Suspense>
                 </div>
             </StylesProvider>
         </BrowserRouter>
